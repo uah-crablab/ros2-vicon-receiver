9183f814941b4d80e0d1c5e2674cddd11d46e3f0
Howard Chen <hc0060@uah.edu>
2023-11-10 12:28:49 -0600

    added libboost_system to install_libs.sh

--- install_libs.sh
+++ install_libs.sh
@@ -10,6 +10,8 @@ sudo cp DataStreamSDK_1.12.0/libboost_timer-mt-x64.so.1.75.0 /usr/lib
 echo "."
 sudo cp DataStreamSDK_1.12.0/libboost_chrono-mt-x64.so.1.75.0 /usr/lib
 echo "."
+sudo cp DataStreamSDK_1.12.0/libboost_system-mt-x64.so.1.75.0 /usr/lib
+echo "."
 sudo chmod 0755 /usr/lib/libViconDataStreamSDK_CPP.so /usr/lib/libboost_system-mt-x64.so.1.75.0 /usr/lib/libboost_thread-mt-x64.so.1.75.0 /usr/lib/libboost_timer-mt-x64.so.1.75.0 /usr/lib/libboost_chrono-mt-x64.so.1.75.0
 echo "." 
 sudo ldconfig
